90a9a9e28ca63451c13d6be558a9bbadfd692797
DagaBhai <dagayash14@gmail.com>
2025-06-01 18:55:19 +0530

    Update Q6.cpp

--- practice/class_practical/cpp/Q6.cpp
+++ practice/class_practical/cpp/Q6.cpp
@@ -1,22 +1,63 @@
-//Write a program to merge two ordered arrays to get a single ordered array.
+//Write a program to merge two ordered arrays to get a single ordered array. 
 
 #include <iostream>
 using namespace std;
 
-int main(){
-    int array1[11] = {1,2,3,4,5,6};
-    int array2[] = {7,8,9,10,11};
-    int i=0;
+void iparr(int arr[], int l){
+    for(int i=0;i<l;++i){
+        cout<<"enter the element for "<<i<<" position";
+        cin>>arr[i];
+    }
+}
 
-    for(i;i<5;++i){
-        array1[i+6]=array2[i];
+void mergearr(int* arr1, int n1, int* arr2, int n2, int* marr) {
+    int i = 0, j = 0, k = 0;
+
+    // Merge while both have elements
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            marr[k++] = arr1[i++];
+        } else {
+            marr[k++] = arr2[j++];
+        }
     }
 
-    int size=sizeof(array1)/sizeof(array1[0]);
-
-    for(int j=0;j<size;++j){
-        cout<<array1[j]<<" ";
+    // Copy remaining elements (if any)
+    while (i < n1) {
+        marr[k++] = arr1[i++];
     }
+    while (j < n2) {
+        marr[k++] = arr2[j++];
+    }
+}
+
+int main() {
+    int n1, n2;
+
+    cout << "Enter size of first sorted array: ";
+    cin >> n1;
+    int* arr1 = new int[n1];
+    iparr(arr1, n1);
+
+    cout << "Enter size of second sorted array: ";
+    cin >> n2;
+    int* arr2 = new int[n2];
+    iparr(arr2, n2);
+
+    int* mergedArr = new int[n1 + n2];
+
+    mergearr(arr1, n1, arr2, n2, mergedArr);
+
+    cout << "Merged sorted array: ";
+    for (int i = 0; i < n1 + n2; ++i) {
+        cout << mergedArr[i] << " ";
+    }
+    cout << endl;
+
+    // Clean up
+    delete[] arr1;
+    delete[] arr2;
+    delete[] mergedArr;
 
     return 0;
 }
