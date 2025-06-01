2bc763c24c3eafdaed0ebeff9d1012a375a4656b
DagaBhai <dagayash14@gmail.com>
2025-06-01 15:44:21 +0530

    Update Q9.cpp

--- practice/class_practical/cpp/Q9.cpp
+++ practice/class_practical/cpp/Q9.cpp
@@ -5,67 +5,81 @@ The class should also have overloaded functions for calculating the area of a ri
 as using Heron's formula to calculate the area of any type of triangle.
 */
 
+
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-class triangle{
-    int b=0,h=0;
-    
-public :
-    float area(int b,int h){
-        float area=0.5*b*h;
-        return area;
-    }
-    float area(int a, int b,int c){
-        float s=(a+b+c)/2.0;
-        float area=(s*(s-a)*(s-b)*(s-c));
-        return sqrt(area);
-    }
+class Triangle{
+    public:
+        int s1,s2,s3,b,h;
+        bool isRightAngled;
+    public:
+        bool input(int a,int b,int c){
+            s1 = a; s2 = b; s3 = c;
+
+
+            if (s1 <= 0 || s2 <= 0 || s3 <= 0) {
+                cout << "All sides must be greater than 0.\n";
+                return false;
+            }
+
+            if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
+                cout << "The sum of any two sides must be greater than the third side.\n";
+                return false;
+            }
+
+            cout << "It is a valid triangle.\n";
+
+            isRightAngled = false;
+            if (pow(s1, 2) == pow(s2, 2) + pow(s3, 2)) {
+                base = min(s2, s3);
+                height = max(s2, s3);
+                isRightAngled = true;
+            } else if (pow(s2, 2) == pow(s1, 2) + pow(s3, 2)) {
+                base = min(s1, s3);
+                height = max(s1, s3);
+                isRightAngled = true;
+            } else if (pow(s3, 2) == pow(s1, 2) + pow(s2, 2)) {
+                base = min(s1, s2);
+                height = max(s1, s2);
+                isRightAngled = true;
+            }
+
+            if (isRightAngled) {
+                cout << "This is a right-angled triangle.\n";
+                cout << "Base = " << base << ", Height = " << height << "\n";
+            }
+
+            return true;
+        }
+
+        float areaRightTriangle() {
+            if (!isRightAngled) {
+                cout << "Not a right-angled triangle.\n";
+                return 0;
+            }
+            return 0.5f * base * height;
+        }
+        
+        float areaHeron() {
+            float s = (s1 + s2 + s3) / 2.0;
+            return sqrt(s * (s - s1) * (s - s2) * (s - s3));
+        }
 };
-
-
-int main() {
+int main(){
+    Triangle t;
     int a, b, c;
 
-    while (true) {
-        cout << "Enter the 1st side: ";
-        cin >> a;
-        cout << "Enter the 2nd side: ";
-        cin >> b;
-        cout << "Enter the 3rd side: ";
-        cin >> c;
+    cout << "Enter three sides of a triangle: ";
+    cin >> a >> b >> c;
 
-        if (a <= 0 || b <= 0 || c <= 0) {
-            cout << "Sides must be greater than 0. Try again.\n";
-        } else if (a + b <= c || b + c <= a || a + c <= b) {
-            cout << "Triangle inequality not satisfied. Try again.\n";
-        } else {
-            break; // valid triangle
-        }
-    }
-
-    triangle triangle1;
-    cout<<"area (Heron's formula): "<<triangle1.area(a,b,c)<<endl;
-    
-    if(pow(a,2)==pow(b,2)+pow(c,2)|| pow(b,2)==pow(a,2)+pow(c,2)||pow(c,2)==pow(a,2)+pow(b,2)){
-
-        int base,height;
-        if (pow(a, 2) == pow(b, 2) + pow(c, 2)){
-            //condition ? value_if_true : value_if_false;
-            base= min(b,c);
-            height = max(b,c);
-        }
-        else if(pow(b, 2) == pow(a, 2) + pow(c, 2)){
-            base= min(a,c);
-            height = max(a,c);
-        }
-        else{
-            base=min(a,b);
-            height = max(a,b);
-        }
-
-        cout<<"area (Right-angled Triangle): "<<triangle1.area(base,height);
+    if (t.input(a, b, c)) {
+        cout << "Area (Heron's Formula): " << t.areaHeron() << "\n";
+        float areaRight = t.areaRightTriangle();
+        if (areaRight > 0)
+            cout << "Area (Right Triangle): " << areaRight << "\n";
     }
 
     return 0;
